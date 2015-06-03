0a89562336327ea19a7e2e84d741733330031c9a
Roberto Beraldo Chaiben <rbchaiben@gmail.com>
2015-06-03 09:40:54 -0300

    add app

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ apps = {
     'SayHi' : 'https://itunes.apple.com/us/app/sayhi-translate/id437818260',
     'Green Kitchen' : 'https://itunes.apple.com/us/app/green-kitchen-healthy-vegetarian/id466252999',
     'Maplets' : 'https://itunes.apple.com/us/app/maplets-by-zaia-design/id337841739',
+    'Relax Melodies Premium' : 'https://itunes.apple.com/br/app/relax-melodies-premium-sleep/id331529422',
+    
 }
 
 # variables JSON Filename
